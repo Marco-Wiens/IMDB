b48a6ef79245bf650a05d2a3d3d3dabdec9ec4b3
Marco-Wiens <marco.wiens@gmail.com>
2023-10-20 14:20:16 +0200

    Import de fs con npm install typescript@latest -g. tsc --init para generar tsconfig.json. Todo esto genera fichero imdbBBDD.json

--- testImdb.ts
+++ testImdb.ts
@@ -1,5 +1,7 @@
 import { Movie } from "./Movie";
 import { Imdb } from "./Imdb";
+import * as fs from 'fs';
+import { log } from "console";
 
 let movie1:Movie = new Movie("Aventuras de Pedrito", 2023, "España", "Ciencia Ficcion");
 let movie2:Movie = new Movie("Romance Down", 2023, "Francia", "Romántica");
@@ -8,4 +10,9 @@ let movie3:Movie = new Movie("Vengadores", 2012, "USA", "Acción");
 let arr:Movie[] = [movie1, movie2, movie3];
 let imdb:Imdb = new Imdb(arr);
 
+let js = JSON.stringify(imdb);
+
+console.log(js);
+
+// fs.writeFileSync("imdbBBDD.json", js);
 
