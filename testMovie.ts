d3f88ec5056c3c5eff79789b868928ea0c1fc877
Marco-Wiens <marco.wiens@gmail.com>
2023-10-20 13:50:43 +0200

    Se crean las clases Movie e Imdb

---
+++ testMovie.ts
@@ -0,0 +1,8 @@
+import { Movie } from "./Movie";
+import { Proffesional } from "./Proffesional";
+
+
+
+let movie1:Movie = new Movie("Las aventuras de Pedrito",2023, "Española", "Cien");
+
+movie1.toString()
